b834ff1dde1b4480b716ac0a1a25a9d90337f454
Michele Bizzoca <michele.bizzoca@proton.me>
2026-08-02 00:40:05 +0200

    minor devops

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ python3 -m venv venv
 . venv/bin/activate
 
 python -m pip install --upgrade pip
-python -m pip install pillow pyvips dacite psutil
+python -m pip install --ignore-installed --no-user pillow pyvips dacite psutil
 
-      DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-   BASHRC="$HOME/.bashrc"
+DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
+BASHRC="$HOME/.bashrc"
 PATH_LINE="export PATH=\"\$PATH:$DIR\""
 
 chmod +x -- "$DIR/anime-ultrascale"
